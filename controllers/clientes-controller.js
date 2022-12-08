497b3222545d82d9f0310303f9f975db30c8efa4
Guillem <saguesguillem@gmail.com>
2022-12-08 09:38:08 +0100

    models.js + async/await clientes-controller

--- controllers/clientes-controller.js
+++ controllers/clientes-controller.js
@@ -1,14 +1,19 @@
-const { Clientes } = require("../models/index");
+const { Clientes, Reservas } = require("../models.js");
 
 const clientesController = {};
 
-clientesController.findAll = (req, res) => {
-  /* GET users listing. */
-  Clientes.findAll().then((data) => {
-    res.send(data);
-  });
-
-  // res.send("respond with a resource");
+/* GET users listing. */
+clientesController.findAll = async (req, res) => {
+  try {
+    const data = await Clientes.findAll({
+      include: { model: Reservas, as: "reservas" },
+    });
+    res.json(data);
+  } catch (error) {
+    res.status(500).send({
+      message: "some error ocurred while retrieving users.",
+    });
+  }
 };
 
 clientesController.findByPk = (req, res) => {
